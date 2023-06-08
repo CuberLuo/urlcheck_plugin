03ed3fb5594814419dd601c4e7afe3a35fb8176f
CuberLuo <1501858187@qq.com>
2023-06-08 19:46:50 +0800

    移除NativeMessaging

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // 监听 chrome.runtime.onMessage 事件
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // 判断消息类型以及其他必要的条件
+  // 判断消息类型
   if (request.action === 'fetch_data') {
     // let url = 'http://121.36.196.242:9998/checkURL'
     let url = 'http://localhost:3000/checkURL'
@@ -16,10 +16,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     })
       .then((response) => {
         if (response.ok) {
-          // 响应成功时执行的操作
           return response.json()
         } else {
-          // 响应出错时执行的操作
           return { success: false, error: '网络请求出错' }
         }
       })
